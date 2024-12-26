7c3749ab17faaef9aa462c04bd234150ed46e7cd
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-12-26 14:49:57 +0200

    Build a basic hovering on 16x16 grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 const container = document.querySelector(".container");
+
+for (i = 0; i < 16*16; i++) {
+    const pixel = document.createElement("div");
+    pixel.setAttribute("style", "height: 30px; width: 30px; border: 2px solid black;")
+    container.appendChild(pixel);
+
+    pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = "black";
+    });
+}
+
+
