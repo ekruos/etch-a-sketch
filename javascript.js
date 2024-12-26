8448d4a39c0d015be528cb01b4a731e697be45a0
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-12-26 16:04:50 +0200

    Add prompt for button

--- javascript.js
+++ javascript.js
@@ -1,13 +1,34 @@
 const container = document.querySelector(".container");
+const pixelButton = document.querySelector(".pixel-button");
 
-for (i = 0; i < 16*16; i++) {
-    const pixel = document.createElement("div");
-    pixel.setAttribute("style", "height: 30px; width: 30px; border: 2px solid black;")
-    container.appendChild(pixel);
+let howMany = 16;
+buildGrids(howMany);
 
-    pixel.addEventListener("mouseover", () => {
-        pixel.style.backgroundColor = "black";
-    });
+pixelButton.addEventListener("click", () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    howMany = +prompt("How many pixels per side do you want? Maximum is 50.");
+    if (howMany > 50) {
+        howMany = 50;
+    }
+    buildGrids(howMany);
+});
+
+
+function buildGrids(howMany) {
+    for (i = 0; i < howMany*howMany; i++) {
+        const pixel = document.createElement("div");
+        pixel.setAttribute(`style`, `height: ${500/howMany}px; width: ${500/howMany}px; border: 1px solid black; box-sizing: border-box;`)
+        container.appendChild(pixel);
+        pixel.style.opacity = 0.9;
+        pixel.addEventListener("mouseover", () => {
+            pixel.style.backgroundColor = "blue";
+            //pixel.style.opacity += 0.9;
+            
+        });
+    }
 }
+
 
 
