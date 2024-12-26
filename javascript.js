cbebed8bda16dd1f05cd204091ca5d832217d4e7
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-12-26 20:31:43 +0200

    Add opacity property for pixels

--- javascript.js
+++ javascript.js
@@ -19,13 +19,16 @@ pixelButton.addEventListener("click", () => {
 function buildGrids(howMany) {
     for (i = 0; i < howMany*howMany; i++) {
         const pixel = document.createElement("div");
-        pixel.setAttribute(`style`, `height: ${500/howMany}px; width: ${500/howMany}px; border: 1px solid black; box-sizing: border-box; opacity: 0.8;`)
+        pixel.setAttribute(`style`, `height: ${500/howMany}px; width: ${500/howMany}px; border: 1px solid black; box-sizing: border-box; opacity: 0.1;`)
         container.appendChild(pixel);
         pixel.addEventListener("mouseover", () => {
             let first = Math.floor(Math.random()*255);
             let second = Math.floor(Math.random()*255);
             let third = Math.floor(Math.random()*255);
             pixel.style.backgroundColor = `rgb(${first}, ${second}, ${third})`;
+            newOpacity = +pixel.style.opacity + 0.1;
+            pixel.style.opacity = `${newOpacity}`;
+
         });
     }
 }
